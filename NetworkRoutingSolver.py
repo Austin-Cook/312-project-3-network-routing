a4c84695aef0c6d71d33b7dff92746ed579c91ae
Austin-Cook <austin.cook.id@gmail.com>
2023-10-07 15:37:43 -0600

    Plan rest of project

--- NetworkRoutingSolver.py
+++ NetworkRoutingSolver.py
@@ -4,17 +4,16 @@
 from CS312Graph import *
 import time
 from abc import ABC, abstractmethod
-from typing import Type
+from math import ceil
 
 
 class NetworkRoutingSolver:
     def __init__(self):
-        # the array of previous nodes from last run of computeShortestPaths()
+        # list of previous nodes from last run of computeShortestPaths()
         # prev: [None, 0, 1, 2, 0, 6, 2, 6]
-        # is    [  -,  a, b, c, a, g, c, g]
         self.prev = None
 
-        # the array of distances to each node from the starting node from last run of computeShortestPaths()
+        # list of distances from the starting node to each node from the last run of computeShortestPaths()
         # dist: [0, 1, 3, 4, 4, 6, 5, 6]
         self.dist = None
 
@@ -23,23 +22,7 @@ class NetworkRoutingSolver:
         self.network = network
 
     def getShortestPath(self, destIndex):
-        self.dest = destIndex
-        # TODO: RETURN THE SHORTEST PATH FOR destIndex
-        #       INSTEAD OF THE DUMMY SET OF EDGES BELOW
-        #       IT'S JUST AN EXAMPLE OF THE FORMAT YOU'LL 
-        #       NEED TO USE
-
-        # # old code
-        # path_edges = []
-        # total_length = 0
-        # node = self.network.nodes[self.source]
-        # edges_left = 3
-        # while edges_left > 0:
-        #     edge = node.neighbors[2]
-        #     path_edges.append((edge.src.loc, edge.dest.loc, '{:.0f}'.format(edge.length)))
-        #     total_length += edge.length
-        #     node = edge.dest
-        #     edges_left -= 1
+        # self.dest = destIndex # TODO DELETEME
 
         # TODO - probably don't need
         # create dictionary for constant edge lookup { node: dictionary { edge_dest_node: edge }}
@@ -81,77 +64,68 @@ class NetworkRoutingSolver:
         return {'cost': total_length, 'path': path_edges}
 
     def computeShortestPaths(self, srcIndex, use_heap=False):
-        self.source = srcIndex
+        # self.source = srcIndex    # TODO Deleteme
         t1 = time.time()
-        # TODO: RUN DIJKSTRA'S TO DETERMINE SHORTEST PATHS.
-        #       ALSO, STORE THE RESULTS FOR THE SUBSEQUENT
-        #       CALL TO getShortestPath(dest_index)
-        self._dijkstra(ArrayQueue)
-        # TODO use proper queue
-
-        print("self.prev: ", self.prev)
-        print("self.dist: ", self.dist)
-
-
+        self.dist, self.prev = _dijkstra(network=self.network, queue_type=ArrayQueue, src_index=srcIndex)
         t2 = time.time()
-        return (t2 - t1)
 
-    def _dijkstra(self, queue_type):
-        """
-        Runs Dijkstra's algorithm to find the shortest path from the node with index
-        srcIndex (0 based) in the network (CS312Graph object) to all other nodes
-        NOTE self.source must be set prior to running this call computeShortestPaths()
+        return t2 - t1
 
-        :param Q: The queue to use in this algorithm
-        :return: 'prev' array from which to derive each path from the starting index
-        """
 
-        # SETUP
+def _dijkstra(network: CS312Graph, queue_type, src_index: int):
+    """
+    Runs Dijkstra's algorithm to find the shortest path from the node with index
+    srcIndex (0 based) in the network (CS312Graph object) to all other nodes
 
-        # get all nodes
-        nodes = self.network.getNodes() # DO NOT MODIFY
+    :param queue_type: The type of queue to use in this algorithm
+    :return: 'prev' array from which to derive each path from the starting index
+    """
 
-        # map nodes to their index for quick lookup
-        node_to_index_map = {}
-        i = 0
-        for u in nodes:                                             # ]- O(n)
-            node_to_index_map[u] = i
-            i += 1
+    # SETUP
 
-        # initialize dist array (for all u in V: dist(u) <- infinity)
-        dist = [float('inf')] * len(nodes)
+    # get all nodes
+    nodes = network.getNodes() # DO NOT MODIFY
 
-        # initialize prev array (for all u in V: prev(u) <- NULL)
-        prev = [None] * len(nodes)
+    # map nodes to their index for quick lookup
+    node_to_index_map = {}
+    i = 0
+    for u in nodes:                                             # ]- O(n)
+        node_to_index_map[u] = i
+        i += 1
 
-        # dist(s) <- 0 # self.source contains the srcIndex
-        dist[self.source] = 0
+    # initialize dist array (for all u in V: dist(u) <- infinity)
+    dist = [float('inf')] * len(nodes)
 
-        # H.makequeue(V) {distances as keys}
-        H = queue_type.make_queue(dist)
+    # initialize prev array (for all u in V: prev(u) <- NULL)
+    prev = [None] * len(nodes)
 
-        # ALGORITHM
+    # dist(s) <- 0 # self.source contains the srcIndex
+    dist[src_index] = 0
 
-        # while H is not empty do
-        while (u_index := H.delete_min()) is not None:
-            # for all edges (u, v) in E do    # just edges from the current node
-            for edge in (nodes[u_index]).neighbors:
-                v_index = node_to_index_map[edge.dest]
+    # H.makequeue(V) {distances as keys}
+    H = queue_type.make_queue(dist)
 
-                assert u_index == node_to_index_map[edge.src]   # TODO Deleteme
+    # ALGORITHM
 
-                # if dist(v) > dist(u) + l(u, v) then
-                alt_path_dist = dist[u_index] + edge.length
-                if dist[v_index] > alt_path_dist:   # TODO How to represent this correctly??
-                    # dist(v) <- dist(u) + l(u, v)
-                    dist[v_index] = alt_path_dist
-                    # prev(v) <- u
-                    prev[v_index] = u_index
-                    # H.decreasekey(v)
-                    H.decrease_weight(v_index, alt_path_dist)
-        # set self.dist and self.prev
-        self.dist = dist
-        self.prev = prev
+    # while H is not empty do
+    while (u_index := H.delete_min()) is not None:
+        # for all edges (u, v) in E do    # just edges from the current node
+        for edge in (nodes[u_index]).neighbors:
+            v_index = node_to_index_map[edge.dest]
+
+            assert u_index == node_to_index_map[edge.src]   # TODO Deleteme
+
+            # if dist(v) > dist(u) + l(u, v) then
+            alt_path_dist = dist[u_index] + edge.length
+            if dist[v_index] > alt_path_dist:
+                # dist(v) <- dist(u) + l(u, v)
+                dist[v_index] = alt_path_dist
+                # prev(v) <- u
+                prev[v_index] = u_index
+                # H.decreasekey(v)
+                H.decrease_weight(v_index, alt_path_dist)
+
+    return dist, prev
 
 
 class QueueInterface(ABC):
@@ -221,7 +195,7 @@ class ArrayQueue(QueueInterface):
     def make_queue(weights: list):
         index_to_weight_map = {}
 
-        # fill the map with index_in_distances->distance_at_index
+        # fill the map with node_id (it's index) -> weight
         for node_index in range(len(weights)):
             index_to_weight_map[node_index] = weights[node_index]
 
@@ -260,14 +234,14 @@ class ArrayQueue(QueueInterface):
 
 class HeapQueue(QueueInterface):
 
-    def __init__(self, heap_array, index_to_weight_map: dict):
+    def __init__(self, heap_array: list, index_to_weight_map: dict):
         """
         Do not call directly; call make_queue() instead.
 
         :param index_to_weight_map: The initial map of 0 based index id mapped to their distances
         """
-        # self.index_to_weight_map = index_to_weight_map
         self.heap_array = heap_array
+        self.index_to_weight_map = index_to_weight_map
 
     def __str__(self):
         pass
@@ -275,28 +249,100 @@ class HeapQueue(QueueInterface):
 
     @staticmethod
     def make_queue(weights: list):
-        pass
-        # make a copy of weights
-        heap_array = weights.copy()
+        # create the array to represent a heap
+        heap_array = weights.copy()                                         # ]- O(n)
 
-        # TODO heapify
-        # _build_min_heap(heap_array) # ]- O(n)
+        # fill the map with node_id (it's initial index) -> weight
+        index_to_weight_map = {}
+        # TODO add
+        for node_index in range(len(weights)):
+            index_to_weight_map[node_index] = weights[node_index]
 
-        # return HeapQueue(heap_array)
+        # create a heap queue with the array and map
+        heap_queue = HeapQueue(heap_array, index_to_weight_map)
+
+        # reorder the heap_array as a min heap
+        heap_queue._build_min_heap()                                        # ]- O(n)
+
+        return heap_queue
 
     def insert(self, node_id: int, weight: float):
+        # append the item to the heap_array
+        # percolate up starting at the index of the item just added (last index)
         pass
 
     def delete_min(self):
+        # set the value of the first item (index 0) to the value of the item at the last index
+        # delete the last item in the array
+        # percolate down from the first item (index 0)
         pass
 
     def decrease_weight(self, node_id: int, new_weight):
+        # get the node's old_weight from the dictionary
+        # update the node's weight in the dictionary
+        # if new_weight < old_weight
+        #   percolate up from node_id
+        # else if new_weight > old_weight
+        #   percolate down at old_weight
         pass
 
-    # heap methods
+    # HEAP METHODS
 
     def _build_min_heap(self):
+        # for each node with at least one child (from high to low)
+        for curr_root_index in range((len(self.heap_array) // 2) - 1, -1, -1):
+            # heapify the subtree
+            self._percolate_down(curr_root_index)
+
+    def _percolate_down(self, curr_index: int):
+        # get left child index
+        # get right child index
+
+        # set min_index to curr_index
+        # set min_val to value at curr_index
+
+        # if left_child_index is not None and value at left_child_index is less than min_val
+        #   set min_index to left_child_index
+        #   set min_val to value at left_child_index
+        # if right child index is not None and value at right_child_index is less than min_val
+        #   set min_index to right_child_index
+        #   set min_val to value at left_child_index
+
+        # if min_index is not curr_index
+        #   swap values at curr_index and min_index
+        #   recursively call _percolate_down on the child's tree
         pass
 
-    def _heapify(self):
+    def _percolate_up(self, curr_index):
+        # if the parent index is greater than the curr_index
+        #   swap values at curr_index and parent_index
+        #   if get the parent index is not None (top of tree)
+        #       recursively call _percolate_up on the parent_index
         pass
+
+    def _get_left_child_index(self, index: int):
+        # gets the index of the left child and returns None if it doesn't exist
+        left_index = (index * 2) + 1
+
+        return left_index if left_index < len(self.heap_array) else None
+
+    def _get_right_child_index(self, index: int):
+        # gets the index of the right child and returns None if it doesn't exist
+        right_index = (index * 2) + 1
+
+        return right_index if right_index < len(self.heap_array) else None
+
+    @staticmethod
+    def _get_parent_index(index: int):
+        # gets the index of the parent and returns None if already at top of tree
+        parent_index = ceil(index / 2) - 1
+
+        return parent_index if parent_index != -1 else None
+
+    def _swap(self, i: int, j: int):
+        # swaps the values in self.heap_array at indexes i and j
+        temp = self.heap_array[i]
+        self.heap_array[i] = j
+        self.heap_array[i] = temp
+
+        # TODO update the map of id->index
